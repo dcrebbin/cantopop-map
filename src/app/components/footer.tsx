6947f71fa273911c8502e37c4ed8efe0f803e45b
Devon Crebbin <devon@land.org.au>
2025-09-11 02:54:39 +0800

    added my email to request locations

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <div className="absolute bottom-0 z-[90] flex h-14 w-full items-center justify-center bg-transparent">
       <a
-        href="https://github.com/dcrebbin/cantopop-map/issues"
+        href="mailto:devon@langpal.com.hk?subject=Cantopop地圖: New Location Submission"
         target="_blank"
         rel="noopener noreferrer"
         className="absolute bottom-0 p-3 font-[Cute] text-xl text-white underline drop-shadow-[0_0_4px_rgba(0,0,0,1)]"
