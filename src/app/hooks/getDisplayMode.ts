b8ef5bf3824b6a4c0dc4bc88755667422e1a9113
Devon Crebbin <devon@land.org.au>
2025-11-14 16:59:36 +0800

    fixed build error

--- src/app/hooks/getDisplayMode.ts
+++ src/app/hooks/getDisplayMode.ts
@@ -1,4 +1,5 @@
 export function getDisplayMode() {
+  if (typeof window === "undefined") return "browser";
   if (window.matchMedia("(display-mode: fullscreen)").matches) {
     return "fullscreen";
   }
