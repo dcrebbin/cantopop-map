25ac1f81273ef97674931a52b5ebcaabbe2af44c
Devon Crebbin <devon@land.org.au>
2026-08-18 13:31:29 +1000

    fixed egg wong title

--- src/app/common/locations.ts
+++ src/app/common/locations.ts
@@ -8014,8 +8014,8 @@ const RAW_LOCATIONS: RawLocationSchema[] = [
     },
   },
   {
-    artists: ["EGG 黃詠霖", "陳健安 On Chan"],
-    name: "沒誰保佑 (Not The Only One)",
+    artists: ["EGG 黃詠霖"],
+    name: "沒誰保佑 (Nobody to bless me)",
     address: "Shing Mun River, New Territories, Hong Kong",
     coordinates: [22.377186, 114.1895227],
     streetView: "https://maps.app.goo.gl/uhpCAQKaMpNsG5jV7",
